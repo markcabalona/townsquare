7afebefd817465792adc434f3dc1664e7c37bba4
markcabalona <work@markcabalona.dev>
2024-11-23 11:30:04 +0800

    #1: Implement AppTheme inherited widget

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:townsquare/core/theme/colors/app_colors.dart';
+import 'package:townsquare/core/theme/text_styles/app_text_styles.dart';
+
+class AppTheme extends InheritedWidget {
+  final AppColors colors;
+  final AppTextStyles textStyles;
+
+  const AppTheme({
+    super.key,
+    required this.colors,
+    required this.textStyles,
+    required super.child,
+  });
+
+  static AppTheme of(BuildContext context) {
+    final instance = context.dependOnInheritedWidgetOfExactType<AppTheme>();
+    if (instance != null) return instance;
+    throw Exception('Could not find AppTheme in context.');
+  }
+
+  @override
+  bool updateShouldNotify(covariant AppTheme oldWidget) {
+    return oldWidget.colors.runtimeType != colors.runtimeType ||
+        oldWidget.textStyles.runtimeType != textStyles.runtimeType;
+  }
+}
