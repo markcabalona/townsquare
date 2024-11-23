fbb25ffca401fa5cc04e2f1feb386ffca2602e86
markcabalona <work@markcabalona.dev>
2024-11-23 17:07:14 +0800

    #5: create initial ActivitiesPage widget

--- lib/core/router/routes/activities_routes.dart
+++ lib/core/router/routes/activities_routes.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:townsquare/core/router/routes/app_routes.dart';
 import 'package:townsquare/core/router/routes/base_routes.dart';
+import 'package:townsquare/features/activities/presentation/pages/activities_page.dart';
 
 class ActivitiesRoutes implements BaseRoutes {
   @override
@@ -10,6 +10,6 @@ class ActivitiesRoutes implements BaseRoutes {
   final RouteBase _activities = GoRoute(
     name: AppRoute.activities.name,
     path: AppRoute.activities.path,
-    builder: (context, state) => const Placeholder(),
+    builder: (context, state) => const ActivitiesPage(),
   );
 }
