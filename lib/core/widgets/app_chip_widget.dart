dee243a4d0dd71f912e8abc74763e76c6182631e
markcabalona <work@markcabalona.dev>
2024-11-28 13:55:28 +0800

    #11: update AppChipWidget to have a .icon factory constructor
     - AppChipWidget.icon returns a chip widget with ad icon only

--- lib/core/widgets/app_chip_widget.dart
+++ lib/core/widgets/app_chip_widget.dart
@@ -1,22 +1,40 @@
-
 import 'package:flutter/material.dart';
 import 'package:townsquare/core/theme/app_theme.dart';
 
 class AppChipWidget extends StatelessWidget {
-  const AppChipWidget({
+  const AppChipWidget._({
     super.key,
     this.backgroundColor,
     this.foregroundColor,
-    required this.label,
+    this.label,
     this.leading,
     this.padding,
+    this.icon,
   });
+  const factory AppChipWidget({
+    Key? key,
+    Color? backgroundColor,
+    Color? foregroundColor,
+    required String label,
+    Widget? leading,
+    EdgeInsetsGeometry? padding,
+  }) = AppChipWidget._;
+
+  const factory AppChipWidget.icon({
+    Key? key,
+    Color? backgroundColor,
+    Color? foregroundColor,
+    required Widget icon,
+    Widget? leading,
+    EdgeInsetsGeometry? padding,
+  }) = AppChipWidget._;
 
   final Color? backgroundColor;
   final Color? foregroundColor;
-  final String label;
+  final String? label;
   final Widget? leading;
   final EdgeInsetsGeometry? padding;
+  final Widget? icon;
 
   @override
   Widget build(BuildContext context) {
@@ -35,24 +53,29 @@ class AppChipWidget extends StatelessWidget {
   }
 
   Widget _buildChild(BuildContext context) {
-    if (leading == null) return _buildLabel(context);
+    if (leading == null) return _buildContent(context);
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
         leading!,
         const SizedBox(width: 2),
-        _buildLabel(context),
+        _buildContent(context),
       ],
     );
   }
 
-  Text _buildLabel(BuildContext context) {
-    return Text(
-      label,
-      style: AppTheme.of(context)
-          .textStyles
-          .body3
-          .copyWith(color: foregroundColor),
+  Widget _buildContent(BuildContext context) {
+    return SizedBox(
+      height: 24,
+      child: label == null
+          ? icon!
+          : Text(
+              label!,
+              style: AppTheme.of(context)
+                  .textStyles
+                  .body3
+                  .copyWith(color: foregroundColor),
+            ),
     );
   }
 }
